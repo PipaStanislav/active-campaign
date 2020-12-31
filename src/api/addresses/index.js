6095980efe08a37bbe090e401d206b5ae3b47990
Pipa Stanislav <pipa.stanislav.work@gmail.com>
2020-12-31 13:51:03 +0200

    Added addresses

---
+++ src/api/addresses/index.js
@@ -0,0 +1,18 @@
+var Address = require('./address');
+
+class ActiveCampaign {
+  constructor({ apiUrl, apiKey }) {
+    this.baseApiUrl = `${apiUrl}/api/3`;
+    this.baseHeaders = { 'Api-Token': apiKey };
+
+    // API`s
+    this.address = new Address({ baseApiUrl: this.baseApiUrl, baseHeaders: this.baseHeaders });
+  }
+}
+
+/**
+ * Module exports.
+ * @public
+ */
+
+module.exports = ActiveCampaign;
