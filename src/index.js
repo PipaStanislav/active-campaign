25de83083149e0171fe24d9111187cd8afc50ef1
Pipa Stanislav <pipa.stanislav.work@gmail.com>
2020-12-31 11:42:29 +0200

    Refactored contact

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-var Contact = require('./contact/contact');
-var Automation = require('./contact/automation');
-var CustomField = require('./contact/custom-field');
-var CustomFieldValues = require('./contact/custom-field-value');
+var Contacts = require('./contact');
 
 class ActiveCampaign {
   constructor({ apiUrl, apiKey }) {
     this.baseApiUrl = `${apiUrl}/api/3`;
     this.baseHeaders = { 'Api-Token': apiKey };
 
-    // Contacts API`s
-    this.contact = new Contact({ baseApiUrl: this.baseApiUrl, baseHeaders: this.baseHeaders });
-    this.automation = new Automation({
-      baseApiUrl: this.baseApiUrl,
-      baseHeaders: this.baseHeaders,
-    });
-    this.customFields = new CustomField({
-      baseApiUrl: this.baseApiUrl,
-      baseHeaders: this.baseHeaders,
-    });
-    this.customFields = new CustomFieldValues({
-      baseApiUrl: this.baseApiUrl,
-      baseHeaders: this.baseHeaders,
-    });
+    // API`s
+    this.contacts = new Contacts({ baseApiUrl: this.baseApiUrl, baseHeaders: this.baseHeaders });
   }
 }
 
